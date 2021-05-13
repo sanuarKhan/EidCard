26342b41ce5e96ae7dee52716e1e3c4c24f4b345
sanuar khan <sanuarkhan220@gmail.conm>
2021-05-13 15:00:49 +0600

    change-4

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,15 @@
 var coverBG = document.getElementById("cover");
 var cardBody = document.getElementById("imgCard");
+var audio = document.getElementById("audio");
 
 function clickMe() {
     cardBody.style.display = "block";
 
     coverBG.style.display = "none";
+    if (clickMe() == true) {
+        audio.attributes.autoplay = true;
+    }
+
     
 }
 
