c7deb1bfe5bf92e5ef37acd421c9322b51c18b82
sanuar khan <sanuarkhan220@gmail.conm>
2021-05-13 12:34:49 +0600

    comment-1

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var coverBG = document.getElementById("cover");
+var cardBody = document.getElementById("imgCard");
+
+function clickMe() {
+    cardBody.style.display = "block";
+
+    coverBG.style.display = "none";
+    
+}
+
